e6365e359eedd3bb47c1b5b91218d3131cfdafd9
quangnd1601 <nduyquang1601@gmail.com>
2026-02-28 01:22:06 +0700

    video 19

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -8,11 +8,31 @@ class MyComponent extends React.Component {
     address: "TP.HCM",
     age: 20,
   };
+  handleMouseOver = (event) => {
+    return console.log(event.pageY);
+  };
+  handleClick = (event) => {
+    console.log("My name is", this.state.name);
+  };
   // JSX: chỉ trả về 1 phần tử thôi
   render() {
     return (
       <div>
         My name is {this.state.name} and My address is {this.state.address}
+        <button
+          onClick={(event) => {
+            return this.handleClick(event);
+          }}
+        >
+          Click Me
+        </button>
+        <button
+          onMouseOver={(event) => {
+            return this.handleMouseOver(event);
+          }}
+        >
+          Hover Me
+        </button>
       </div>
     );
   }
