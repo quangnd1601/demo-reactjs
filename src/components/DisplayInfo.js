c1182185fd2ce4fdbb234d6dd08c532d8d6e44ae
quangnd1601 <nduyquang1601@gmail.com>
2026-03-01 18:40:47 +0700

    video 24 - hien thi danh sach

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -3,12 +3,19 @@ import React from "react";
 class DisplayInfo extends React.Component {
   render() {
     console.log(this.props);
-    const { name, age } = this.props;
+    const { listUsers } = this.props; // Object
     // props => viết tắt properties (tài sản)
     return (
       <div>
-        <div>My name's {name}</div>
-        <div>My age's {age}</div>
+        {listUsers.map((user) => {
+          return (
+            <div key={user.id}>
+              <div>My name's {user.name}</div>
+              <div>My age's {user.age}</div>
+              <hr />
+            </div>
+          );
+        })}
       </div>
     );
   }
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -6,15 +6,22 @@ import React from "react";
 import UserInfo from "./UserInfo";
 import DisplayInfo from "./DisplayInfo";
 class MyComponent extends React.Component {
+  state = {
+    listUsers: [
+      { id: 1, name: "Quang", age: "30" },
+      { id: 2, name: "Beso", age: "32" },
+      { id: 3, name: "Quang Beso", age: "21" },
+    ],
+  };
   // JSX: chỉ trả về 1 phần tử thôi
   render() {
-    const myAge = ["a", "b", "C"];
+    // DRY: don't repeat yourself
     return (
       <div>
         <UserInfo></UserInfo>
-        <br /> <br />
-        <DisplayInfo name="Quang1" age={myAge}></DisplayInfo>
-        <DisplayInfo name={"BESO"} age={20}></DisplayInfo>
+        <br />
+        <DisplayInfo listUsers={this.state.listUsers}></DisplayInfo>
+        <hr />
       </div>
     );
   }
