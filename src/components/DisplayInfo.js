812622116ebf30e11ec4bc72fec6c6c49f9fdfbb
quangnd1601 <nduyquang1601@gmail.com>
2026-03-01 18:20:29 +0700

    video 23- props

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -3,12 +3,12 @@ import React from "react";
 class DisplayInfo extends React.Component {
   render() {
     console.log(this.props);
-
+    const { name, age } = this.props;
     // props => viết tắt properties (tài sản)
     return (
       <div>
-        <div>My name's {this.props.name}</div>
-        <div>My age's {this.props.age}</div>
+        <div>My name's {name}</div>
+        <div>My age's {age}</div>
       </div>
     );
   }
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -8,12 +8,13 @@ import DisplayInfo from "./DisplayInfo";
 class MyComponent extends React.Component {
   // JSX: chỉ trả về 1 phần tử thôi
   render() {
+    const myAge = ["a", "b", "C"];
     return (
       <div>
         <UserInfo></UserInfo>
         <br /> <br />
-        <DisplayInfo name="Quang1" age={21}></DisplayInfo>
-        <DisplayInfo name="BESO" age={20}></DisplayInfo>
+        <DisplayInfo name="Quang1" age={myAge}></DisplayInfo>
+        <DisplayInfo name={"BESO"} age={20}></DisplayInfo>
       </div>
     );
   }
