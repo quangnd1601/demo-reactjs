11759ecb14b8d5ae9e6d6c594ed2e1ac95f284b7
quangnd1601 <nduyquang1601@gmail.com>
2026-03-08 15:27:08 +0700

    video 28 - img

--- src/components/DisplayInfo.js
+++ src/components/DisplayInfo.js
@@ -1,5 +1,7 @@
 import React from "react";
-import "./DisplayInfo.scss";
+import "./DisplayInfo.scss"; /* style={{ color: "blue", paddingTop: "20px" }} */
+import logo from "../logo.svg";
+
 class DisplayInfo extends React.Component {
   state = {
     isShowListUser: true,
@@ -13,19 +15,20 @@ class DisplayInfo extends React.Component {
     const { listUsers } = this.props;
     return (
       <div className="display-info-container">
+        <img src={logo}></img>
+
         <button onClick={() => this.handleShowHide()}>
           {this.state.isShowListUser === true
             ? "Hide List Users"
             : "Show List Users"}
         </button>
+
         {this.state.isShowListUser && (
           <div>
             {listUsers.map((user) => {
               return (
                 <div key={user.id} className={+user.age > 18 ? "green" : "red"}>
-                  <div style={{ color: "blue", paddingTop: "20px" }}>
-                    My name's {user.name}
-                  </div>
+                  <div>My name's {user.name}</div>
                   <div>My age's {user.age}</div>
                   <hr />
                 </div>
